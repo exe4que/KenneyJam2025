726761e4e04a1ef83338911e1e1b85e6d1260f73
Exequiel <exe4que@gmail.com>
2025-07-20 12:08:12 -0300

    reload scene added + health bar script fixed

--- Assets/Scripts/UI/HealthBarUI.cs
+++ Assets/Scripts/UI/HealthBarUI.cs
@@ -66,7 +66,7 @@ public class PlayerHealthUI : MonoBehaviour
 
         if (_healthSlider != null)
         {
-            _healthSlider.value = 100f;
+            _healthSlider.value = playerHealth.MaxHealth;
         }
      
     }
